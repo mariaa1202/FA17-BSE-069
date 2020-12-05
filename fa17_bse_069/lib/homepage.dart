9b9f376a39095b016976a8d56e3f4b126f396970
mariaa1202 <mariaaslam1202@gmail.com>
2020-12-05 15:59:35 +0500

    dice app

--- fa17_bse_069/lib/homepage.dart
+++ fa17_bse_069/lib/homepage.dart
@@ -4,8 +4,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.teal,
+      appBar: AppBar(
+        title: Text('Dice App'),
+      ),
       body: Center(
-        child: Text('Hello To Home Page'),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Image(
+                  image: new AssetImage('images/dice1.jpg'),
+                ),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Image(
+                  image: new AssetImage('images/dice2.jpg'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
