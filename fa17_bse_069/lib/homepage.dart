2ff932bed0c4c266c177d23735cc36a158a551df
mariaa1202 <mariaaslam1202@gmail.com>
2020-12-05 16:31:21 +0500

    Dice App Run

--- fa17_bse_069/lib/homepage.dart
+++ fa17_bse_069/lib/homepage.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int left_image = 1;
+  int right_image = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,20 +22,26 @@ class HomePage extends StatelessWidget {
             Expanded(
               child: FlatButton(
                 onPressed: () {
-                  print('Dice1');
+                  setState(() {
+                    left_image = Random().nextInt(5) + 1;
+                  });
+                  print('value $left_image');
                 },
                 child: Image(
-                  image: new AssetImage('images/dice1.jpg'),
+                  image: new AssetImage('images/dice$left_image.jpg'),
                 ),
               ),
             ),
             Expanded(
               child: FlatButton(
                 onPressed: () {
-                  print('Dice2');
+                  setState(() {
+                    right_image = Random().nextInt(5) + 1;
+                  });
+                  print('value $right_image');
                 },
                 child: Image(
-                  image: new AssetImage('images/dice2.jpg'),
+                  image: new AssetImage('images/dice$right_image.jpg'),
                 ),
               ),
             ),
