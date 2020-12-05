89a4af492296c1fd7beb85a9ae0eee703dc85024
mariaa1202 <mariaaslam1202@gmail.com>
2020-12-05 18:15:13 +0500

    Update homepage.dart

--- fa17_bse_069/lib/homepage.dart
+++ fa17_bse_069/lib/homepage.dart
@@ -16,36 +16,94 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Dice App'),
       ),
-      body: Center(
-        child: Row(
-          children: <Widget>[
-            Expanded(
-              child: FlatButton(
-                onPressed: () {
-                  setState(() {
-                    left_image = Random().nextInt(5) + 1;
-                  });
-                  print('value $left_image');
-                },
-                child: Image(
-                  image: new AssetImage('images/dice$left_image.jpg'),
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 70.0,
+          ),
+          Center(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        left_image = Random().nextInt(5) + 1;
+                      });
+                      print('value $left_image');
+                    },
+                    child: Image(
+                      image: new AssetImage('images/dice$left_image.jpg'),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        right_image = Random().nextInt(5) + 1;
+                      });
+                      print('value $right_image');
+                    },
+                    child: Image(
+                      image: new AssetImage('images/dice$right_image.jpg'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 170.0,
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => ContactUs()));
+            },
+            child: Text('Contact Us'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ContactUs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 30.0,
+              ),
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('images/a.jpg'),
+                radius: 70.0,
+              ),
+              Text(
+                'Maria Aslam',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
                 ),
               ),
-            ),
-            Expanded(
-              child: FlatButton(
-                onPressed: () {
-                  setState(() {
-                    right_image = Random().nextInt(5) + 1;
-                  });
-                  print('value $right_image');
-                },
-                child: Image(
-                  image: new AssetImage('images/dice$right_image.jpg'),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30.0,
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
